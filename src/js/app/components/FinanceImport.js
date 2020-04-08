92f4181005afcb9d115924c56d7821e88946bb27
Danilo Vitoriano <vitoriano08@gmail.com>
2020-04-08 13:00:02 -0300

    add heroku api

--- src/js/app/components/FinanceImport.js
+++ src/js/app/components/FinanceImport.js
@@ -4,7 +4,7 @@ export default () => {
     // eslint-disable-next-line no-unused-vars
     const [state, dispatch] = useContext(FinanceContext)
     const sendRequest = async () => {
-        const payload = await fetch("http://localhost:3920/financas/anterior")
+        const payload = await fetch("https://evening-badlands-20922.herokuapp.com/financas/semana")
             .then(res => res.json())
             .then(res => {
                 const payload = res.map(finance => {
